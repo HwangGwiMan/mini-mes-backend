1d9cebd58a63f613c377cb3ac9c1a08f680c8f79
HwangGwiMan <eze886@daum.net>
2026-02-21 14:56:19 +0900

    프로젝트 초기화

---
+++ src/main/java/com/github/gwiman/mini_mes_backend/MiniMesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.gwiman.mini_mes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gwiman/mini_mes_backend/MiniMesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gwiman.mini_mes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
